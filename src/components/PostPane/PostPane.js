2054b6a5d5bf9f9ba57385029424d3f6511a0eb4
Banukaiw <banukaiw@gmail.com>
2024-09-13 22:06:36 +0530

    body development

--- src/components/PostPane/PostPane.js
+++ src/components/PostPane/PostPane.js
@@ -4,7 +4,7 @@ import "./postpane.css";
 export default function PostPane() {
   return (
     <div className="postpanebox1">
-      <div className="stories">
+      {/* <div className="stories">
         <div className="carts1">
           <div className="logo1">            
             <div class="StoryLogo"></div>
@@ -41,11 +41,11 @@ export default function PostPane() {
           </div>
           <div className="storyname">Rishi Bro</div>
         </div>
-      </div>
+      </div> */}
 
       
 
-      <div class="story-section">
+      {/* <div class="story-section">
         <div class="story create-story">
           <div class="story-image"></div>
           <div class="story-text">Create story</div>
@@ -55,10 +55,11 @@ export default function PostPane() {
             <img src="profile1.jpg" alt="ProfilePicture"></img>
           </div>
           <div class="story-content">
-            <img src="story1.jpg" alt="Story"></img>
+            <img src="1.jpg" alt="Story"></img>
           </div>
           <div class="story-name">Ahinsa Sewwandi</div>
         </div>
+
         <div class="story">
           <div class="story-profile">
             <img src="profile2.jpg" alt="ProfilePicture"></img>
@@ -68,6 +69,7 @@ export default function PostPane() {
           </div>
           <div class="story-name">Sandani Fernando</div>
         </div>
+
         <div class="story">
           <div class="story-profile">
             <img src="profile3.jpg" alt="ProfilePicture"></img>
@@ -77,8 +79,30 @@ export default function PostPane() {
           </div>
           <div class="story-name">Patta Comments</div>
         </div>
-      </div>
-      <div className="postpanebox">hi</div>
+      </div> */}
+
+
+        <div className="StorySection">
+          <div className="Story CreateStory">
+            <div className="Storyimage"></div>
+            <div className="StoryFont">
+              <div className="CreateButton">+</div>
+              <div className="CreateText">Create Story</div>
+            </div>
+          </div>
+
+          <div className="Story">
+            <div className="StoryProfile">
+             <img src="./images/1.jpg" alt="Story"></img>
+            </div>
+            <div className="StoryContent">
+            <img src="./images/story1.jpg" alt="Story"></img>
+            </div>
+            <div className="StoryName">Banuka Ishara</div>
+          </div>
+          <div className="Story"></div>
+          <div className="Story"></div>
+        </div>
 
       
     </div>
